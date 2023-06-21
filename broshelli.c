60eeec6a1088d5e68e5355ba00c71f8be4aeec81
Chubak Bidpaa <chubakbidpaa@gmail.com>
2023-06-21 13:30:49 +0430

    added json reader

--- broshelli.c
+++ broshelli.c
@@ -1,10 +1,16 @@
 #ifndef MAX_CORE_SIZE
-#define MAX_CORE_SIZE 10000200
+#define MAX_CORE_SIZE 100
 #endif
+
+#define TAG(CHAR) (CHAR == L'%')
+#define VAL_TERM(CHAR) (CHAR == L',' || CHAR == L'}')
+#define NEXT(STREAM) *STREAM++
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
+#include <string.h>
+#include <stdarg.h>
 #include <unistd.h>
 #include <wchar.h>
 #include <sys/types.h>
@@ -12,7 +18,7 @@
 typedef wchar_t btychar_t;
 typedef wchar_t *btystr_t;
 typedef uint64_t btyint_t;
-typedef uint16_t btyptyid_t;
+typedef uint32_t btyptyid_t;
 typedef double btyflt_t;
 typedef uint32_t btymsglen_t;
 
@@ -21,30 +27,86 @@ typedef enum NativeMsgType {
 	Directive,
 } btymsgtyy_t;
 
-typedef struct BtyNativeMessage {
-	btystr_t core[MAX_CORE_SIZE];
-	btyptyid_t ptyid;
-	btyymsgtyy_t type;
-	btymsglen_t length;
+typedef union BtyNativeMessage {
+	char arena[MAX_CORE_SIZE];
+	struct {
+		btyptyid_t ptyid;
+		btymsgtyy_t type;
+		btystr_t core;
+		union {
+			char diff[4];
+			btymsglen_t whole;
+		} length;
+	} data;	
 } btycontext_t;
 
 
-static inline void allcoate_new_message_arena(btycontext_t *context) {
-	context = calloc(1, sizeof(btycontext_t));
+static inline void allcoate_new_message_context(btycontext_t **context) {
+	*context = calloc(1, sizeof(btycontext_t));
 }
 
-static inline void deallocate_message_arena(btycontext_t *context) { 
-	free(context); 
+static inline void deallocate_message_context(btycontext_t **context) { 
+	free(*context); 
 }
 
 static inline void reset_message_context(btycontext_t *context) {  
 	memset(context, 0, sizeof(btycontext_t));  
 }
 
-static inline void read_stdin_to_context(btycontext_t *context) {  
-	fwscanf(stdin, "%u{\"type\":%d,\"core\":\"%s\",\"ptyid\":%hu}", context->length, context->type, context->core, context->ptyid);
+static void read_wide_json(FILE *file, wchar_t *fmt, ...) {
+	wchar_t chr, fchr;
+	va_list argls;
+	va_start(argls, fmt);
+	while (fchr = NEXT(fmt)) {
+		if (TAG(fchr)) {
+			fchr = NEXT(fmt);
+			switch(fchr) {
+			case L'i':
+				uint64_t num = 0, *numptr = va_arg(argls, uint64_t*);
+				do {
+					chr = fgetwc(file);
+					if (VAL_TERM(chr))
+						break;
+					num = ((num << 3) + (num << 1)) + (chr - L'0');
+				} while (1);
+				*numptr = num;
+				break;
+			case L'b':
+				char *bytes = va_arg(argls, char*);
+				do {
+					chr = fgetc(file);
+					if (VAL_TERM(chr))
+						break;
+					*(bytes++) = chr;
+				} while (1);
+				break;
+			case L's':
+				wchar_t *wstring = va_arg(argls, wchar_t*);
+				wchar_t oldchr;
+				do {
+					chr = fgetwc(file);
+					if (VAL_TERM(chr) && oldchr == L'"')
+						break;
+					*(wstring++) = chr;
+					oldchr = chr;
+				} while (1);
+				break;
+			default:
+				break;
+		}
+	} else {
+		fgetwc(file);
+	}
+}
+	va_end(argls);
 }
 
-static inline void write_context_to_stdout(btycontext_t *context) {
-	fwprintf(stdout, "%u{\"core\":\"%s\"}", context->core);
+int main() {
+	btycontext_t *context;
+	int ret;
+	uint64_t tst;
+	//allcoate_new_message_context(&context);
+	read_wide_json(stdin, L"{%i}", &tst);
+	//deallocate_message_context(&context);
+
 }
