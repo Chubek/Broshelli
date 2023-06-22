3ba2f571ded0b62249c994a9710301ff6dda8530
Chubak Bidpaa <chubakbidpaa@gmail.com>
2023-06-22 14:39:11 +0430

    improvements and refactors and changes

--- brotty.h
+++ brotty.h
@@ -28,25 +28,29 @@ typedef struct {
 	int slave;
 	FILE *mstream;
 	FILE *sstream;
-} btypty_t;
+} bpty_t;
 
-static inline open_pty_device(btypty_t *pty) {
+static inline open_pty_device_pair(bpty_t *pty) {
 	openpty(&pty->master, &pty->slave, NULL, NULL);
 	pty->mstream = fdopen(pty->master, "w");
 	pty->sstream = fdopen(pty->slave, "w");
 }
 
-static inline write_to_master(btypty_t *pty, char *data, char term) {
+static inline close_pty_device_pair(bpty_t *pty) {
+	close(pty->master);
+	close(pty->slave);
+}
+
+static inline write_to_master(bpty_t *pty, char *data, char term, size_t maxlen) {
 	char chr;
-	while ((chr = fputc(*dst++, pty->mstream)) != term);
+	while (((chr = fputc(*dst++, pty->mstream)) != term) && --maxlen);
 	return chr;
 }
 
-static inline read_from_slave(btypty_t *pty, char *data, char term) {
+static inline read_from_slave(bpty_t *pty, char *data, char term, size_t maxlen) {
 	char chr;
-	while (chr = (*(data++) = fgetc(pty->sstream)) != term);
+	while ((chr = (*(data++) = fgetc(pty->sstream)) != term) && --maxlen);
 	return chr;
 }
-
 
 #endif
