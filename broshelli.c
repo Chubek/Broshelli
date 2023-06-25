b122073e335f13026f44735caa8e4872d32e4f48
Chubak Bidpaa <chubakbidpaa@gmail.com>
2023-06-25 19:46:36 +0430

    progress in making master writer

--- broshelli.c
+++ broshelli.c
@@ -27,36 +27,60 @@ int open_pty_pair(ptypair_r ppair) {
 }
 
 int fork_off_slave_and_exec(char *slavefname) {
+	RELAY_SIGNUM_TO_PARENT(SIGTRAP, SIGNUM_EXECVE_SUCCESS);
 	char shellpath[SHELL_PATH_LEN], *shell = &shellpath[0];
 	int slvfd, slvfdcp1, slvfdcp2, slvfdcp3, ret;
 	if ((ret = slvfd = open(slavefname, O_RDWR)) < 0) {
-		SEND_SIGNAL_TO_PARENT(SIGNAL_SLVFD_OPEN, int, ret);
+		SEND_SIGNAL_TO_PARENT(SIGNUM_SLVFD_OPEN, int, ret);
 		_exit(EXIT_FAILURE);
 	}
 	slvfdcp1 = dup(slavefd);
 	slvfdcp2 = dup(slavefd);
 	slvfdcp3 = dup(slavefd);
 	if ((ret = dup2(STDIN_FD, slvfdcp1)) < 0) {
-		SEND_SIGNAL_TO_PARENT(SIGNAL_SLVFD_DUP, int, ret);
+		SEND_SIGNAL_TO_PARENT(SIGNUM_SLVFD_DUP, int, ret);
 		_exit(EXIT_FAILURE);
 	}
 	if ((ret = dup2(STDOUT_FD, slvfdcp2)) < 0)
-		SEND_SIGNAL_TO_PARENT(SIGNAL_SLVFD_DUP, int, ret);
+		SEND_SIGNAL_TO_PARENT(SIGNUM_SLVFD_DUP, int, ret);
 		_exit(EXIT_FAILURE);
 	if ((ret = dup2(STDERR_FD, slvfdcp3)) < 0)
-		SEND_SIGNAL_TO_PARENT(SIGNAL_SLVFD_DUP, int, ret);
+		SEND_SIGNAL_TO_PARENT(SIGNUM_SLVFD_DUP, int, ret);
 		_exit(EXIT_FAILURE);
 	if (!(shell = getenv(SHELL_ENV))) {
-		SEND_SIGNAL_TO_PARENT(SIGNAL_SHELL_EXEC, int, SIGNAL_SHELL_DFL);
+		SEND_SIGNAL_TO_PARENT(SIGNUM_SHELL_EXEC, int, SIGNUM_SHELL_DFL);
 		shell = SHELL_DFL;
 	}
 	if ((ret = execlp(shell, shell, (char*)NULL)) < 0) {
-		SEND_SIGNAL_TO_PARENT(SIGNAL_SHELL_EXEC, int, ret);
+		SEND_SIGNAL_TO_PARENT(SIGNUM_SHELL_EXEC, int, ret);
 		_exit(EXIT_FAILURE);
 	}
 }
 
-int handle_new_terminal() {
+void wait_for_messages_from_main(pid_t cpid, FILE *masterfstm) {
+	int ret;
+	ssize_t iolen;
+	mqd_t mqdesc;
+	char mqname[MQUEUE_NAME_LEN], shellio[SHELL_IO_LEN];
+	if ((ret = snprintf(&mqname[0], MQUEUE_NAME_LENGHT, "%d.btymq", mypid)) < 0) {
+		SEND_SIGNAL_TO_PARENT(SIGNUM_MQUEUE_NAME, int, ret);
+		_exit(EXIT_FAILURE);
+	}
+	SEND_SIGNAL_TO_PARENT(SIGNUM_MQUEUE_NAME, int, ret);
+	mqdesc = mq_open(mqname, O_RDWR);
+	while (FOREVER) {
+
+	}
+}
+
+void sigrt_handle_termination(int signum, siginfo_t *signalinfo, void *unused) {
+	if (signum == SIGNUM_TERMINATE_MASTER) {
+		close(signalinfo->si_value.sival_int);
+		_exit(EXIT_SUCCESS);
+	}
+}
+
+void handle_new_terminal(int masterfdnotify) {
 	pid_t cpid;
 	int ret, pipefd[2];
 	ptypair_t ppair;
@@ -65,23 +89,33 @@ int handle_new_terminal() {
 		SEND_SIGNAL_TO_PARENT(SIGNUM_PTY_OPEN, int, ret);
 		_exit(EXIT_FAILURE);
 	}
+	write(masterfdnotify, &ppair->masterfd, sizeof(int));
+	close(masterfdnotify);
 	SEND_SIGNAL_TO_PARENT(SIGNUM_PTY_OPEN, int, ret);
 	if ((ret = cpid = fork()) < 0) {
-		SEND_SIGNAL_TO_PARENT(SIGNAL_FORK_CHILD, int, ret);
+		SEND_SIGNAL_TO_PARENT(SIGNUM_FORK_CHILD, int, ret);
 		_exit(EXIT_FAILURE);
 	}
 	if (!cpid) {
 		char slavefname[SLVFNAME_LEN];
 		memset(&slavefname[0], 0, SLVGNAME_LEN);
 		close(pipefd[1]);
-		read(&slavefname[0], pipefd[0], SLVFNAME_LEN);
+		read(pipefd[0], &slavefname[0], SLVFNAME_LEN);
 		fork_off_slave_and_exec(&slavefname[0]);
 	} else {
-		SEND_SIGNAL_TO_PARENT(SIGNAL_FORK_CHILD, int, cpid);
+		SET_RT_SIGNAL_ACTION(SIGNUM_TERMINATE_MASTER, sigrt_handle_termination);
 		siginfo_t signalinfo;
 		memset(&signalinfo, 0, sizeof(siginfo_t));
-		WAIT_FOR_SIGNALS(&signalinfo, SIGNAL_SLVFD_OPEN, SIGNAL_SLVFD_DUP, SIGNAL_SHELL_EXEC);
-		
+		SEND_SIGNAL_TO_PARENT(SIGNUM_FORK_CHILD, int, cpid);
+		close(pipefd[0]);
+		write(pipefd[1], &ppair.slavefname[0], SLVFNAME_LEN);
+		WAIT_FOR_SIGNALS(&signalinfo, MAX_SIGRT_WAITNS, SIGNUM_SLVFD_OPEN, SIGNUM_SLVFD_DUP, SIGNUM_SHELL_EXEC, SIGNUM_EXECVE_SUCCESS);
+		UNTRACE_CHILD_PROCESS(cpid);
+		SEND_SIGNAL_TO_PARENT(signalinfo.si_signo, int, signalinfo.si_value.sival_int);
+		WAIT_FOR_SIGNALS(&signalinfo, MAX_SIGRT_WAITNS, SIGCONT, SIGTERM);
+		if (signalinfo.si_signo == SIGTERM)
+			_exit(EXIT_FAILURE);
+		wait_for_messages_from_main(cpid, ppair.masterfstm);
 	}
 }
 
--- broshelli.h
+++ broshelli.h
@@ -11,12 +11,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <sys/types.h>
-#include <termios.h>
 #include <unistd.h>
-#include <wchar.h>
 #include <time.h>
-
+#include <sys/types.h>
+#include <sys/ptrace.h>
+ #include <sys/stat.h>
 
 #define STDIN_FD fileno(stdin)
 #define STDOUT_FD fileno(stdout)
@@ -30,17 +29,27 @@
 #define SHELL_PATH_LEN 256
 #endif
 
-#ifndef ARENA_SIZE
-#define ARENA_SIZE 256000
+#ifndef MQUEUE_NAME_LEN
+#define MQUEUE_NAME_LEN 512
+#endif
+
+#ifndef SHELL_IO_LEN
+#define SHELL_IO_LEN 256000
 #endif
 
 #ifndef SHELL_ENV
 #define SHELL_ENV "SHELL"
 #endif
 
+#ifndef MAX_SIGRT_WAITNS
+#define MAX_SIGRT_WAITNS 5000
+#endif
+
 #ifndef SHELL_DFL
 #define SHELL_DFL "/usr/bin/bash"
 #endif
+
+#define FOREVER 1
 
 #define SIGVAL_SHELL_DFL 7
 
@@ -50,12 +59,15 @@
 #define SIGNUM_FORK_CHILD SIGRTMIN + 3
 #define SIGNUM_SLVFD_DUP SIGRTMIN + 4
 #define SIGNUM_SLVFD_OPEN SIGRTMIN + 5
+#define SIGNUM_EXECVE_SUCCESS SIGRTMIN + 6
+#define SIGNUM_MQUEUE_NAME SIGRTMIN + 7
+#define SIGNUM_TERMINATE_MASTER SIGRTMIN + 8
 
-#define WAIT_FOR_SIGNALS(INFO, SECS, NSECS, ...)                                             \
+#define WAIT_FOR_SIGNALS(INFO, NSECS, ...)                                             \
   do {                                                                         \
     char signal, signals[] = {__VA_ARGS__, 0};                                 \
     struct sigset_t signalset;                                                 \
-    struct tspec =  (struct timespec){.tv_sec = SECS, .tv_nsec = NSECS}		   \
+    struct tspec =  (struct timespec){.tv_sec = 0, .tv_nsec = NSECS}		   \
     sigfillset(&signalset);                                                    \
     while ((signal = *signals++))                                              \
       sigaddset(&signalset, (int)signal);                                      \
@@ -66,8 +78,23 @@
   sigqueue(PROCNUM, SIGNUM, (union sigval){.sival_##SVALTYY = SIGVAL})
 #define SEND_SIGNAL_TO_PARENT(...) SEND_SIGNAL(getppid(), __VA_ARGS__)
 #define RAISE_SIGNAL_TO_SELF(...) SEND_SIGNAL(getpid(), __VA_ARGS__)
+#define RELAY_SIGNAL_TO_PARENT(SIGNUM, RELAY_SIGNUM) 							\
+  do {																			\
+  	void signal_handler(int signum) {											\
+  		SEND_SIGNAL_TO_PARENT(signum, int, RELAY_SIGNUM);						\
+  	}																			\
+	struct sigaction signalaction =  (struct sigaction){.sa_handler = &signal_handler};	\
+	sigaction(SIGNUM, &signalaction, NULL);												 \
+  } while (0)
+#define SET_RT_SIGNAL_ACTION(SIGNUM, SA_HANDLER_NAME)							\
+  do {																					\
+  	struct sigaction signalaction = (struct sigaction){.sa_handler = &SA_HANDLER_NAME, .sa_flags = SA_SIGINFO};	\
+  	sigaction(SIGNUM, &signalaction, NULL);												\
+  } while (0)
 
-#define NEW_SIGINFO(HANDLE_PTR)
+#define TRACE_CURR_PROCESS() ptrace(PTRACE_TRACEME, NULL, NULL, NULL)
+#define UNTRACE_CHILD_PROCESS(PID) ptrace(PTRAC_DETACH, PID, NULL, NULL)
+
 
 typedef enum {
   INIT_TERM,
@@ -89,7 +116,7 @@ typedef struct {
   pid_t ptypid;
   uint32_t msglen;
   relaydir_t directive;
-  char io[ARENA_SIZE];
+  char io[SHELL_IO_LEN];
 } iorelay_t;
 
 #endif
