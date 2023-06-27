7c307cddfa646561f8c73cdc7b61f796650c2791
Chubak Bidpaa <chubakbidpaa@gmail.com>
2023-06-27 22:26:22 +0430

    applied macros from .h and also some refactors

--- broshelli.c
+++ broshelli.c
@@ -51,21 +51,21 @@ void wait_for_messages_from_main(pid_t cpid, FILE *masterfstm) {
 	size_t shloutlen;
 	mqd_t mqdesc;
 	char mqname[MQUEUE_NAME_LEN] = { 0 }, shellio[SHELL_IO_LEN] = { 0 };
-	if ((ret = snprintf(&mqname[0], MQUEUE_NAME_LEN, "%d.btymq", mypid)) < 0) {
+	if ((ret = GET_MQ_NAME(&mqname[0], cpid)) < 0) {
 		SEND_SIGNAL_TO_PARENT(SIGNUM_MQUEUE_NAME, int, ret);
 		_exit(EXIT_FAILURE);
 	}
 	SEND_SIGNAL_TO_PARENT(SIGNUM_MQUEUE_NAME, int, MQUEUE_FNAME_SUCCESS);
 	mqdesc = mq_open(mqname, O_RDWR);
 	while (IS_NOT_FLAG(shellio, FLAG_TERM_MASTER)) {
-		memset(&shellio[0], 0, SHELL_IO_LEN);
+		ZERO_OUT(&shellio[0], SHELL_IO_LEN);
 		if ((ret = iolen = mq_receive(mqdesc, &shellio[0], SHELL_IO_LEN, NULL)) < 0) {
 			SEND_SIGNAL_TO_PARENT(SIGNUM_MQUEUE_RECEIVE, int, ret);
 			continue;
 		}
 		SEND_SIGNAL_TO_PARENT(SIGNUM_MQUEUE_RECEIVE, int, MESSAGE_RECEIVE_SUCCESS);
 		fwrite(&shellio[0], sizeof(char), iolen, masterfstm);
-		memset(&shellio[0], 0, iolen);
+		ZERO_OUT(&shellio[0], iolen);
 		while ((shloutlen = fread(&shellio[0], sizeof(char), SHELL_IO_LEN, masterfstm))) {
 			mq_send(mqdesc, &shellio[0], shloutlen, NULL);
 		}
@@ -83,9 +83,13 @@ void sigrt_handle_termination(int signum, siginfo_t *signalinfo, void *unused) {
 
 void handle_new_terminal(int masterfdnotify) {
 	pid_t cpid;
-	int ret, pipefd[2];
+	int ret, pipefd[2] = { 0 };
 	ptypair_t ppair;
-	memset(&ppair, 0, sizeof(ptypair_t));
+	ZERO_OUT(&ppair, sizeof(ptypair_t));
+	if ((ret = pipe(&pipefd[0])) < 0) {
+		SENT_SIGNAL_TO_PARENT(SIGNUM_PIPE_OPEN, int, ret);
+	}
+	SENT_SIGNAL_TO_PARENT(SIGNUM_PIPE_OPEN, int, PIPE_OPEN_SUCCESS);
 	if ((ret = open_pty_pair(&ppair)) < 0) {
 		SEND_SIGNAL_TO_PARENT(SIGNUM_PTY_OPEN, int, ret);
 		_exit(EXIT_FAILURE);
@@ -99,14 +103,14 @@ void handle_new_terminal(int masterfdnotify) {
 	}
 	if (!cpid) {
 		char slavefname[SLVFNAME_LEN];
-		memset(&slavefname[0], 0, SLVGNAME_LEN);
+		ZERO_OUT(&slavefname[0], SLVGNAME_LEN);
 		close(pipefd[1]);
 		read(pipefd[0], &slavefname[0], SLVFNAME_LEN);
 		fork_off_slave_and_exec(&slavefname[0]);
 	} else {
 		SET_RT_SIGNAL_ACTION(SIGNUM_TERMINATE_MASTER, sigrt_handle_termination);
 		siginfo_t signalinfo;
-		memset(&signalinfo, 0, sizeof(siginfo_t));
+		ZERO_OUT(&signalinfo, sizeof(siginfo_t));
 		SEND_SIGNAL_TO_PARENT(SIGNUM_FORK_CHILD, int, cpid);
 		close(pipefd[0]);
 		write(pipefd[1], &ppair.slavefname[0], SLVFNAME_LEN);
@@ -121,9 +125,12 @@ void handle_new_terminal(int masterfdnotify) {
 }
 
 
-int create_new_terminal(int retctx[RETCTX_NUM]) {
+int create_new_terminal(unsigned long retctx[RETCTX_NUM]) {
 	pid_t cpid, shellpid;
-	int ret, pipefd[2];
+	int ret, pipefd[2] = { 0 };
+	if ((ret = pipe(&pipefd[0])) < 0) {
+		return SANITIZE_PIPE_ERR_RETURN(ret);
+	}
 	if ((ret = cpid = form()) < 0)
 		return ret;
 	if (!cpid) {
@@ -134,10 +141,10 @@ int create_new_terminal(int retctx[RETCTX_NUM]) {
 		close(pipefd[1]);
 		read(pipefd[0], &masterfd, sizeof(int));
 		struct siginfo signalinfo;
-		WAIT_FOR_SIGNALS(&signalinfo, SIGNUM_FORK_CHILD, MAX_SIGRT_WAITNS, SIGNUM_SLVFD_OPEN, SIGNUM_SLVFD_DUP, SIGNUM_SHELL_EXEC, SIGNUM_EXECVE_SUCCESS);
-		if (signalinfo.si_value.int_val < 0) {
-			return SANITIZE_SIG_RETURN(signalinfo);
-		} else if (signalinfo.si_no == SIGNUM_FORK_CHILD) {
+		WAIT_FOR_SIGNALS(&signalinfo, SIGNUM_PIPE_OPEN, SIGNUM_FORK_CHILD, MAX_SIGRT_WAITNS, SIGNUM_SLVFD_OPEN, SIGNUM_SLVFD_DUP, SIGNUM_SHELL_EXEC, SIGNUM_EXECVE_SUCCESS);
+		if (SIG_VAL < 0) {
+			return SANITIZE_SIG_ERR_RETURN(signalinfo);
+		} else if (SIG_NUM == SIGNUM_FORK_CHILD && SIG_VAL >= 0) {
 			shellpid = signalinfo.si_value.si_int;
 		}
 		ASSIGN_RETCTX(retctx, cpid, shellpid, masterfd, signalinfo.si_no, signalinfo.si_value.si_int);
@@ -153,24 +160,24 @@ int handle_pty_io(pid_t procid, int masterfd, char *swap) {
 	int ret;
 	size_t iolen;
 	mqd_t mqdesc;
-	char mqname[MQUEUE_NAME_LEN], *swapptr = &swap[0];
+	char mqname[MQUEUE_NAME_LEN] = { 0 }, *swapptr = &swap[0];
 	struct siginfo signalinfo;
-	memset(&mqname[0], 0, MQUEUE_NAME_LEN);
-	if ((ret = snprintf(&mqname[0], MQUEUE_NAME_LENGHT, "%d.btymq", procid)) < 0) {
+	if ((ret = GET_MQ_NAME(&mqname[0], procid)) < 0) {
 		return MQNAME_FAIL_RET;
 	}
+	ZERO_OUT(swapptr, SWAP_AREA_LEN);
 	while (IS_NOT_FLAG(swap, FLAG_TERM_PTY)) {
 		if (IS_FLAG(swap, FLAG_NEW_CMD)) {
 			iolen = 0;
-			memmove(&iolen, &swapptr[1], sizeof(size_t));
+			DATA_COPY(&iolen, &swapptr[1], sizeof(size_t));
 			if ((ret = mq_send(mqdesc, &shellio[1 + sizeof(size_t)], iolen, NULL)) < 0)
 				return ret;
-			memset(&signalinfo, 0, sizeof(struct siginfo));
+			ZERO_OUT(&signalinfo, sizeof(struct siginfo));
 			WAIT_FOR_SIGNALS(&signalinfo, SIGNUM_MQUEUE_RECEIVE);
-			if (signalinfo.si_value.sival_int < 0) {
+			if (SIG_VAL < 0) {
 				return PTERM_LOOP_FAIL;
-			} else if (signalinfo.si_value.sival_int != MESSAGE_RECEIVE_SUCCESS) {
-				memmove(swapptr, MSG_DELIVER_ERR_MSG_TXT, MSG_DELIVER_ERR_MSG_LEN);
+			} else if (SIG_VAL != MESSAGE_RECEIVE_SUCCESS) {
+				DATA_COPY(swapptr, MSG_DELIVER_ERR_MSG_TXT, MSG_DELIVER_ERR_MSG_LEN);
 			}
 			*swapptr = (char)FLAG_SEND_OUT;
 			if ((ret = mq_receive(mqdesc, &swapptr[1], SWAP_AREA_LEN, NULL)))
