923402776bdba67eadaf52bc3781c3b810d1a1e6
Chubak Bidpaa <chubakbidpaa@gmail.com>
2023-06-29 09:58:09 +0430

    restructure

--- include/gendef.h
+++
@@ -1,8 +0,0 @@
-#ifndef GENDEF_H
-#define GENDEF_H
-
-typedef long yield_t;
-typedef int fdsc_t;
-
-
-#endif
--- include/ptydef.h
+++ include/ptydef.h
@@ -1,13 +1,17 @@
 #ifndef PTYDEF_H
 #define PTYDEF_H
 
-#ifndef 
+#define _XOPEN_SOURCE  600
+#define _GNU_SOURCE
+#include <stdlib.h>
+#include <stio.h>
+#include <unistd.h>
 
-
-
-
+typedef int trialnum_t;
 typedef int ptydsc_t;
+typedef char ptychr_t;
 typedef char ptyname_t[PTY_NAME_LEN_MAX];
+
 
 typedef struct PseudoTerminal {
   ptydsc_t fdsc;
